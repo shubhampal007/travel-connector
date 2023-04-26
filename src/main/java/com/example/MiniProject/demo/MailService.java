fe3384558452ec5a70635e1106d3031d5e7df46c
Shubham Pal <shubham.pal@decimal.co.in>
2023-04-26 17:15:33 +0530

    Email send API

---
+++ src/main/java/com/example/MiniProject/demo/MailService.java
@@ -0,0 +1,26 @@
+package com.example.MiniProject.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String username;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        message.setFrom(username);
+        javaMailSender.send(message);
+    }
+}
+
---
+++ src/main/java/com/example/MiniProject/demo/controller/SendEmailController.java
@@ -0,0 +1,28 @@
+package com.example.MiniProject.demo.controller;
+
+import com.example.MiniProject.demo.CommonController;
+import com.example.MiniProject.demo.EmailRequest;
+import com.example.MiniProject.demo.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("email")
+public class SendEmailController extends CommonController {
+
+    @Autowired
+    private MailService mailService;
+
+    @PostMapping("/sendEmail")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        String to = request.getTo();
+        String subject = request.getSubject();
+        String body = request.getBody();
+        mailService.sendEmail(to, subject, body);
+        return ResponseEntity.ok("Email sent successfully");
+    }
+}
